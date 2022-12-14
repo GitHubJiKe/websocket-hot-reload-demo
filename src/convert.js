317dc9d60ab316e7852f054ad30e3e6db21b447d
苑朋飞 <yuanpengfei@yuanpengfeideMacBook-Pro-2.local>
2022-12-14 21:20:27 +0800

    use markdown-it

--- src/convert.js
+++ src/convert.js
@@ -1,4 +1,9 @@
 const showdown = require("showdown");
+const md = require("markdown-it")({
+    html: true,
+    linkify: true,
+    typographer: true,
+});
 const fs = require("fs");
 const constant = require("./constant");
 const { JSDOM } = require("jsdom");
@@ -7,8 +12,12 @@ globalThis.window = new JSDOM("", {}).window;
 
 const convert = new showdown.Converter(constant.showdownOpts);
 
-const convertFunc = (filepath) =>
-    convert.makeHtml(fs.readFileSync(filepath).toString());
+const convertFunc = (filepath, mode = "markdown-it") => {
+    const contentStr = fs.readFileSync(filepath).toString();
+    return mode === "showdown"
+        ? convert.makeHtml(contentStr)
+        : md.render(contentStr);
+};
 
 const convert2Markdown = (src) => convert.makeMarkdown(src);
 
